2bddc9d9ade96310e044d32b639d534d85bfc023
Alex Baumgartner <baumi.dev@gmail.com>
2025-05-21 16:02:44 +1000

    Automated starter-kit export

--- export/config/statamic/eloquent-driver.php
+++ export/config/statamic/eloquent-driver.php
@@ -6,74 +6,74 @@ return [
     'table_prefix' => env('STATAMIC_ELOQUENT_PREFIX', ''),
 
     'asset_containers' => [
-        'driver' => 'eloquent',
+        'driver' => env('STATAMIC_DRIVER', 'file'),
         'model' => \Statamic\Eloquent\Assets\AssetContainerModel::class,
     ],
 
     'assets' => [
-        'driver' => 'file',
+        'driver' => env('STATAMIC_DRIVER', 'file'),
         'model' => \Statamic\Eloquent\Assets\AssetModel::class,
         'asset' => \Statamic\Eloquent\Assets\Asset::class,
     ],
 
     'blueprints' => [
-        'driver' => 'file',
+        'driver' => env('STATAMIC_DRIVER', 'file'),
         'model' => \Statamic\Eloquent\Fields\BlueprintModel::class,
         'namespaces' => 'all',
     ],
 
     'collections' => [
-        'driver' => 'file',
+        'driver' => env('STATAMIC_DRIVER', 'file'),
         'model' => \Statamic\Eloquent\Collections\CollectionModel::class,
         'update_entry_order_queue' => 'default',
         'update_entry_order_connection' => 'default',
     ],
 
     'collection_trees' => [
-        'driver' => 'file',
+        'driver' => env('STATAMIC_DRIVER', 'file'),
         'model' => \Statamic\Eloquent\Structures\TreeModel::class,
         'tree' => \Statamic\Eloquent\Structures\CollectionTree::class,
     ],
 
     'entries' => [
-        'driver' => 'file',
-        'model' => \Statamic\Eloquent\Entries\EntryModel::class,
+        'driver' => env('STATAMIC_DRIVER', 'file'),
+        'model' => \Statamic\Eloquent\Entries\UuidEntryModel::class,
         'entry' => \Statamic\Eloquent\Entries\Entry::class,
         'map_data_to_columns' => false,
     ],
 
     'fieldsets' => [
-        'driver' => 'file',
+        'driver' => env('STATAMIC_DRIVER', 'file'),
         'model' => \Statamic\Eloquent\Fields\FieldsetModel::class,
     ],
 
     'forms' => [
-        'driver' => 'file',
+        'driver' => env('STATAMIC_DRIVER', 'file'),
         'model' => \Statamic\Eloquent\Forms\FormModel::class,
     ],
 
     'form_submissions' => [
-        'driver' => 'file',
+        'driver' => env('STATAMIC_DRIVER', 'file'),
         'model' => \Statamic\Eloquent\Forms\SubmissionModel::class,
     ],
 
     'global_sets' => [
-        'driver' => 'file',
+        'driver' => env('STATAMIC_DRIVER', 'file'),
         'model' => \Statamic\Eloquent\Globals\GlobalSetModel::class,
     ],
 
     'global_set_variables' => [
-        'driver' => 'file',
+        'driver' => env('STATAMIC_DRIVER', 'file'),
         'model' => \Statamic\Eloquent\Globals\VariablesModel::class,
     ],
 
     'navigations' => [
-        'driver' => 'file',
+        'driver' => env('STATAMIC_DRIVER', 'file'),
         'model' => \Statamic\Eloquent\Structures\NavModel::class,
     ],
 
     'navigation_trees' => [
-        'driver' => 'file',
+        'driver' => env('STATAMIC_DRIVER', 'file'),
         'model' => \Statamic\Eloquent\Structures\TreeModel::class,
         'tree' => \Statamic\Eloquent\Structures\NavTree::class,
     ],
@@ -84,22 +84,22 @@ return [
     ],
 
     'taxonomies' => [
-        'driver' => 'file',
+        'driver' => env('STATAMIC_DRIVER', 'file'),
         'model' => \Statamic\Eloquent\Taxonomies\TaxonomyModel::class,
     ],
 
     'terms' => [
-        'driver' => 'file',
+        'driver' => env('STATAMIC_DRIVER', 'file'),
         'model' => \Statamic\Eloquent\Taxonomies\TermModel::class,
     ],
 
     'tokens' => [
-        'driver' => 'file',
+        'driver' => env('STATAMIC_DRIVER', 'file'),
         'model' => \Statamic\Eloquent\Tokens\TokenModel::class,
     ],
 
     'sites' => [
-        'driver' => 'file',
+        'driver' => env('STATAMIC_DRIVER', 'file'),
         'model' => \Statamic\Eloquent\Sites\SiteModel::class,
     ],
 ];
